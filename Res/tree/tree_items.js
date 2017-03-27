3b7247645bbcfdd2eca80ce63ea7cd13c879c5e6
yanzhibin <969674788@qq.com>
2017-03-27 10:33:24 +0800

    maven
    maven

---
+++ Res/tree/tree_items.js
@@ -0,0 +1,28 @@
+var TREE_ITEMS = [
+	['Home','index.html',{'sb':' '},
+		['1. maven初识', 'maven.html',{'tt':'1. maven初识','sb':'maven.html'},
+			['1.1 maven介绍', 'maven(2).html',{'tt':'1.1 maven介绍','sb':'maven(2).html'},],
+			['1.2 maven下载', 'maven(3).html',{'tt':'1.2 maven下载','sb':'maven(3).html'},],
+			['1.3 maven环境变量配置', 'maven(4).html',{'tt':'1.3 maven环境变量配置','sb':'maven(4).html'},],
+],
+		['2. maven 常用命令', 'maven(5).html',{'tt':'2. maven 常用命令','sb':'maven(5).html'},
+			['2.1 总结', '无标题.html',{'tt':'2.1 总结','sb':'无标题.html'},],
+			['2.2 maven坐标', 'maven(6).html',{'tt':'2.2 maven坐标','sb':'maven(6).html'},],
+			['2.3 maven仓库', 'maven(7).html',{'tt':'2.3 maven仓库','sb':'maven(7).html'},],
+			['2.4 maven生命周期', 'maven(8).html',{'tt':'2.4 maven生命周期','sb':'maven(8).html'},],
+],
+		['3. eclipse maven插件', 'eclipsemaven.html',{'tt':'3. eclipse maven插件','sb':'eclipsemaven.html'},
+			['3.1 插件安装', '无标题(2).html',{'tt':'3.1 插件安装','sb':'无标题(2).html'},],
+			['3.2 插件配置', '无标题(3).html',{'tt':'3.2 插件配置','sb':'无标题(3).html'},],
+			['3.3 创建工程', '无标题(4).html',{'tt':'3.3 创建工程','sb':'无标题(4).html'},],
+			['3.4 插件操作对应maven命令关系', 'maven(9).html',{'tt':'3.4 插件操作对应maven命令关系','sb':'maven(9).html'},],
+			['3.5 依赖，聚合，继承', '无标题(5).html',{'tt':'3.5 依赖，聚合，继承','sb':'无标题(5).html'},],
+			['3.6 pom.xml详解', 'pom.xml.html',{'tt':'3.6 pom.xml详解','sb':'pom.xml.html'},],
+],
+		['4. maven实战技巧', 'maven(10).html',{'tt':'4. maven实战技巧','sb':'maven(10).html'},
+			['4.1 搭建标准前后台项目结构', '无标题(6).html',{'tt':'4.1 搭建标准前后台项目结构','sb':'无标题(6).html'},],
+			['4.2 阿里云远程仓库', '无标题(7).html',{'tt':'4.2 阿里云远程仓库','sb':'无标题(7).html'},],
+],
+
+]
+];
